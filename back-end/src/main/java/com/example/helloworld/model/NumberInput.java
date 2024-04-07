0324367510318f1cb9d5e4e0f468b78bc3c8afd6
higgs <1909015791@qq.com>
2024-04-07 19:37:57 +0800

    购买会员接口初步实现

--- back-end/src/main/java/com/example/helloworld/model/NumberInput.java
+++ back-end/src/main/java/com/example/helloworld/model/NumberInput.java
@@ -1,22 +1,17 @@
-<script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
-</head>
-<body>
-<div id="map" style="height: 400px;"></div>
-<button onclick="uploadRandomPath()">上传并显示随机路径</button>
-<form id="uploadForm">
-    <input type="text" name="routeName" placeholder="路线名称">
-    <input type="file" name="routeFile">
-    <button type="submit">上传</button>
-</form>
-var userId = 2;a) {
-        this.a = a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public void setB(int b) {
-        this.b = b;
+method: 'POST',
+body: JSON.stringify({
+    userId: userId,
+    routeName: '随机路径23',
+    routeData: geojson
+}),
+headers: {
+    'Content-Type': 'application/json'
+}
+})
+.then(response => response.text())
+.then(data => {
+    alert(data);
+    getGpsRoutes(userId);
+});
     }
 }
