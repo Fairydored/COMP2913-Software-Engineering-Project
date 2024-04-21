138ad74e53b94a7921d238ce585810436aef7622
higgs <1909015791@qq.com>
2024-04-21 20:13:26 +0800

    根据群组id返回用户信息

--- back-end/src/main/java/com/example/helloworld/model/NumberInput.java
+++ back-end/src/main/java/com/example/helloworld/model/NumberInput.java
@@ -3,6 +3,3 @@ body: JSON.stringify({
     userId: userId,
 
     public User(int id,String username,String password){
-        this.id = id;
-        this.username = username;
-        this.password = password;
