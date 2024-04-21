cd0ffc512f2e639cbce9a354b5abdccf4a7a20f9
higgs <1909015791@qq.com>
2024-04-21 20:11:44 +0800

    群组搜索接口

--- back-end/src/main/java/com/example/helloworld/model/NumberInput.java
+++ back-end/src/main/java/com/example/helloworld/model/NumberInput.java
@@ -1,3 +1,11 @@
 method: 'POST',
 body: JSON.stringify({
     userId: userId,
+
+    public User(int id,String username,String password){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId(){
